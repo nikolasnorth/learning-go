2c1361f4e0ccb5a24a904bf46aa8b4273b835621
Nikolas North <nikolasn09@gmail.com>
2022-01-11 15:23:21 -0500

    implement simple cli calculator

--- src/simplemath/simplemath.go
+++ src/simplemath/simplemath.go
@@ -1,1 +1,65 @@
-package simplemath
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func ParseInputToFloat(a string, b string) (float64, float64, error) {
+	x, err1 := strconv.ParseFloat(a, 64)
+	y, err2 := strconv.ParseFloat(b, 64)
+	if err1 != nil || err2 != nil {
+		return 0, 0, errors.New("could not convert at least one input value to valid float")
+	}
+	return x, y, nil
+}
+
+func Calc(op string, a float64, b float64) (float64, error) {
+	switch op {
+	case "add":
+		return a + b, nil
+	case "subtract":
+		return a - b, nil
+	case "multiply":
+		return a * b, nil
+	case "divide":
+		if b == 0 {
+			return 0, errors.New("divisor cannot be zero")
+		}
+		return a / b, nil
+	default:
+		return 0, errors.New("invalid operator")
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 3 {
+		errMsg := `invalid number of arguments (found %d, need 3)
+format should follow: [operator] [x] [y]
+example:
+> add 1 2
+> subtract 2 1
+> multiply 2 2
+> divide 4 2`
+		err := fmt.Errorf(errMsg, len(args))
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	a, b, err := ParseInputToFloat(args[1], args[2])
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	result, err := Calc(args[0], a, b)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println(result)
+}
---
+++ src/simplemath/simplemath_test.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"log"
+	"testing"
+)
+
+func TestParseInputToFloat(t *testing.T) {
+	t.Run("with two floats", func(t *testing.T) {
+		expectedA := 2.0
+		expectedB := 1.15
+		actualA, actualB, err := ParseInputToFloat("2.0", "1.15")
+		if err != nil {
+			log.Fatalf("got an error but didn't expect one %q", err.Error())
+		}
+		if actualA != expectedA {
+			log.Fatalf("got %f want %f", actualA, expectedA)
+		}
+		if actualB != expectedB {
+			log.Fatalf("got %f want %f", actualB, expectedB)
+		}
+	})
+	t.Run("with one float and one integer", func(t *testing.T) {
+		expectedA := 2.3
+		expectedB := 2.0
+		actualA, actualB, err := ParseInputToFloat("2.3", "2")
+		if err != nil {
+			log.Fatalf("got an error but didn't expect one %q", err.Error())
+		}
+		if actualA != expectedA {
+			log.Fatalf("got %f want %f", actualA, expectedA)
+		}
+		if actualB != expectedB {
+			log.Fatalf("got %f want %f", actualB, expectedB)
+		}
+	})
+	t.Run("with two integers", func(t *testing.T) {
+		expectedA := 3.0
+		expectedB := 2.0
+		actualA, actualB, err := ParseInputToFloat("3", "2")
+		if err != nil {
+			log.Fatalf("got an error but didn't expect one %q", err.Error())
+		}
+		if actualA != expectedA {
+			log.Fatalf("got %f want %f", actualA, expectedA)
+		}
+		if actualB != expectedB {
+			log.Fatalf("got %f want %f", actualB, expectedB)
+		}
+	})
+}
+
+func TestCalc(t *testing.T) {
+	t.Run("with invalid operator", func(t *testing.T) {
+		_, err := Calc("invalidop", 1, 1)
+		if err == nil {
+			log.Fatal("expected an error but didn't get one")
+		}
+	})
+
+	t.Run("adding", func(t *testing.T) {
+		t.Run("two integers", func(t *testing.T) {
+			expected := 120.0
+			actual, err := Calc("add", 100, 20)
+			if err != nil {
+				log.Fatalf("got an error but didn't expect one %q", err.Error())
+			}
+			if actual != expected {
+				log.Fatalf("got %f want %f", actual, expected)
+			}
+		})
+
+		t.Run("one integer and one float", func(t *testing.T) {
+			expected := 120.0
+			actual, err := Calc("add", 100, 20.0)
+			if err != nil {
+				log.Fatalf("got an error but didn't expect one %q", err.Error())
+			}
+			if actual != expected {
+				log.Fatalf("got %f want %f", actual, expected)
+			}
+		})
+
+		t.Run("two floats", func(t *testing.T) {
+			expected := 120.0
+			actual, err := Calc("add", 100.0, 20.0)
+			if err != nil {
+				log.Fatalf("got an error but didn't expect one %q", err.Error())
+			}
+			if actual != expected {
+				log.Fatalf("got %f want %f", actual, expected)
+			}
+		})
+	})
+
+	t.Run("subtracting", func(t *testing.T) {
+		t.Run("two integers", func(t *testing.T) {
+			expected := 100.0
+			actual, err := Calc("subtract", 150, 50)
+			if err != nil {
+				log.Fatalf("got an error but didn't expect one %q", err.Error())
+			}
+			if actual != expected {
+				log.Fatalf("got %f want %f", actual, expected)
+			}
+		})
+
+		t.Run("one integer and one float", func(t *testing.T) {
+			expected := 100.0
+			actual, err := Calc("subtract", 150, 50.0)
+			if err != nil {
+				log.Fatalf("got an error but didn't expect one %q", err.Error())
+			}
+			if actual != expected {
+				log.Fatalf("got %f want %f", actual, expected)
+			}
+		})
+
+		t.Run("two floats", func(t *testing.T) {
+			expected := 100.0
+			actual, err := Calc("subtract", 150.0, 50.0)
+			if err != nil {
+				log.Fatalf("got an error but didn't expect one %q", err.Error())
+			}
+			if actual != expected {
+				log.Fatalf("got %f want %f", actual, expected)
+			}
+		})
+	})
+
+	t.Run("multiplying", func(t *testing.T) {
+		t.Run("two integers", func(t *testing.T) {
+			expected := 100.0
+			actual, err := Calc("multiply", 10, 10)
+			if err != nil {
+				log.Fatalf("got an error but didn't expect one %q", err.Error())
+			}
+			if actual != expected {
+				log.Fatalf("got %f want %f", actual, expected)
+			}
+		})
+
+		t.Run("one integer and one float", func(t *testing.T) {
+			expected := 100.0
+			actual, err := Calc("multiply", 10, 10.0)
+			if err != nil {
+				log.Fatalf("got an error but didn't expect one %q", err.Error())
+			}
+			if actual != expected {
+				log.Fatalf("got %f want %f", actual, expected)
+			}
+		})
+
+		t.Run("two floats", func(t *testing.T) {
+			expected := 100.0
+			actual, err := Calc("multiply", 10.0, 10.0)
+			if err != nil {
+				log.Fatalf("got an error but didn't expect one %q", err.Error())
+			}
+			if actual != expected {
+				log.Fatalf("got %f want %f", actual, expected)
+			}
+		})
+	})
+
+	t.Run("dividing", func(t *testing.T) {
+		t.Run("with a zero divisor", func(t *testing.T) {
+			_, err := Calc("divide", 100, 0)
+			if err == nil {
+				log.Fatal("expected an error but didn't get one")
+			}
+		})
+
+		t.Run("two integers", func(t *testing.T) {
+			expected := 10.0
+			actual, err := Calc("divide", 100, 10)
+			if err != nil {
+				log.Fatalf("got an error but didn't expect one %q", err.Error())
+			}
+			if actual != expected {
+				log.Fatalf("got %f want %f", actual, expected)
+			}
+		})
+
+		t.Run("one integer by one float", func(t *testing.T) {
+			expected := 10.0
+			actual, err := Calc("divide", 100, 10.0)
+			if err != nil {
+				log.Fatalf("got an error but didn't expect one %q", err.Error())
+			}
+			if actual != expected {
+				log.Fatalf("got %f want %f", actual, expected)
+			}
+		})
+
+		t.Run("two floats", func(t *testing.T) {
+			expected := 10.0
+			actual, err := Calc("divide", 100.0, 10.0)
+			if err != nil {
+				log.Fatalf("got an error but didn't expect one %q", err.Error())
+			}
+			if actual != expected {
+				log.Fatalf("got %f want %f", actual, expected)
+			}
+		})
+	})
+}
